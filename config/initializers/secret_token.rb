107ddff7cd2ab87d83f399b5048e3ae0d89867e8
Anthony Lassiter <anthonylassiter@gmail.com>
2013-02-11 23:26:20 -0600

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitube::Application.config.secret_token = '877c59ef47af3261927ff16974015fabb20ac29c6db797863f90fdea8b6176c3dd47563df149de5878d5a25e85f2cf86465b710efdf2fb133a7dd09a185b29a2'

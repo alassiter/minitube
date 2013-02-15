f3da5ffa26c39832d046e61275703b4cb77168b0
Anthony Lassiter <anthonylassiter@gmail.com>
2013-02-15 10:00:29 -0600

    Started changing the way the videos are queried.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,10 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all(:include => {:releases => [:country,:policy]})
+    # @videos = Video.all(:include => {:releases => [:country,:policy]})
+    @releases = Release.joins(:video,:country,:policy).select("
+        releases.id,videos.id as vid, videos.name as vname, countries.name as cname, policies.id as pid")
+
+    @videos = @releases.group_by
   end
 
   def show
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   has_many :releases
   has_many :countries, through: :releases
   attr_accessible :name
+
+  def self.releases
+      joins(:releases => [:policy, :country]).select("videos.id,videos.name,policies.id,countries.code").group("videos.id,policies.id").order("videos.id,countries.code")
+  end
 end

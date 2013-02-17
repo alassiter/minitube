f74b0e8f09815ac3981022d5578ea6eb3c28419a
Anthony Lassiter <anthonylassiter@gmail.com>
2013-02-17 17:23:54 -0600

    Moved country link to a helper

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,6 @@
 module VideosHelper
+    def country_link(release,country_idx)
+        country = (country_idx == 0 ? release['country'] : " - " + release['country'])
+        link_to country, country_path(release['country'])
+    end
 end
--- app/models/release.rb
+++ app/models/release.rb
@@ -8,9 +8,11 @@ class Release < ActiveRecord::Base
     scope :free, lambda { where(:policy_id => 2) }
     scope :monetize, lambda { where(:policy_id => 3) }
 
+    scope :from_country, lambda {|country_id| where("country_id = ?",country_id)}
+
     def self.all
         # redefining all method on this model.
         @releases = Release.joins(:video,:country,:policy).select("
-        releases.id,videos.id as vid, videos.name as vname, countries.code as country, releases.country_id as cid, policies.id as pid").order("vid")
+            releases.id,videos.id as vid, videos.name as vname, countries.code as country, releases.country_id as cid, policies.id as pid").order("vid")
     end
 end

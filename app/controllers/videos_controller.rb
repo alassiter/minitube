708af2cf5ab9dfcd0e125723a06c82f0034c7aa0
Anthony Lassiter <anthonylassiter@gmail.com>
2013-02-17 15:32:55 -0600

    Moved some code to a model. Build a hash table with results

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,14 @@
 class VideosController < ApplicationController
   def index
     # @videos = Video.all(:include => {:releases => [:country,:policy]})
-    @releases = Release.joins(:video,:country,:policy).select("
-        releases.id,videos.id as vid, videos.name as vname, countries.name as cname, policies.id as pid")
-
-    @videos = @releases.group_by
+    releases = Release.all
+    videos = releases.group_by{|r| {'vid' => r.vid, 'vname' => r.vname}}
+    grouped = {}
+    videos.each do |k,v|
+        grouped[k["vid"]] = {"name" => k["vname"], "releases" => v.group_by(&:pid)}
+    end
+    @policies = Policy.all
+    @videos = grouped
   end
 
   def show
--- app/models/release.rb
+++ app/models/release.rb
@@ -7,4 +7,10 @@ class Release < ActiveRecord::Base
     scope :blocked, lambda { where(:policy_id => 1) }
     scope :free, lambda { where(:policy_id => 2) }
     scope :monetize, lambda { where(:policy_id => 3) }
+
+    def self.all
+        # redefining all method on this model.
+        @releases = Release.joins(:video,:country,:policy).select("
+        releases.id,videos.id as vid, videos.name as vname, countries.code as country, releases.country_id as cid, policies.id as pid").order("vid")
+    end
 end

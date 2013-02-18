5fdec507e4401cb10d070a640826acfa1330754b
Anthony Lassiter <anthonylassiter@gmail.com>
2013-02-18 08:55:58 -0600

    limited returned records in order to track down heroku issue

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,7 +8,7 @@ class VideosController < ApplicationController
         grouped[k["vid"]] = {"name" => k["vname"], "releases" => v.group_by(&:pid)}
     end
     @policies = Policy.all
-    @videos = grouped
+    @videos = grouped.first(10)
   end
 
   def show
